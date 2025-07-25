4813e390035bf353383c805ce4d7c38e491578db
Binkyshark <pinkyshark97531@gmail.com>
2025-07-25 17:47:47 +0300

    Added countByCity and countByType endpoints in hotel controller

--- src/controllers/hotel.controller.js
+++ src/controllers/hotel.controller.js
@@ -30,3 +30,42 @@ export const getHotels = async (req, res, next) => {
         next(err);
     }
 };
+
+export const countByCity = async (req, res) => {
+  const cities = req.query.cities?.split(',');
+
+  try {
+    const list = await Promise.all(
+      cities.map(city => Hotel.countDocuments({ city: city.trim() }))
+    );
+
+    res.status(200).json(
+      cities.map((city, index) => ({
+        city: city.trim(),
+        count: list[index]
+      }))
+    );
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const countByType = async (req, res) => {
+  try {
+    const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+    const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+    const resortCount = await Hotel.countDocuments({ type: "resort" });
+    const villaCount = await Hotel.countDocuments({ type: "villa" });
+    const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+    res.status(200).json([
+      { type: "hotel", count: hotelCount },
+      { type: "apartment", count: apartmentCount },
+      { type: "resort", count: resortCount },
+      { type: "villa", count: villaCount },
+      { type: "cabin", count: cabinCount },
+    ]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- src/routes/hotel.routes.js
+++ src/routes/hotel.routes.js
@@ -3,10 +3,13 @@ import { createHotel, getHotels } from '../controllers/hotel.controller.js';
 import {authMiddleware} from '../middlewares/auth.middleware.js';
 import { isAdmin }   from '../middlewares/isAdmin.middleware.js';
 import { uploadCloud } from '../middlewares/multer.js';
+import { countByCity, countByType } from "../controllers/hotel.controller.js";
 
 const router = express.Router();
 
 router.post('/', authMiddleware , isAdmin ,uploadCloud.single("photo") ,createHotel);
 router.get('/' , getHotels); 
+router.get("/count-by-city", countByCity);
+router.get("/count-by-type", countByType);
 
 export default router;
